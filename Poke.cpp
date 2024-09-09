8d632f41d504e996d9d38c9f7d3d4a8b0b902367
jeon sungil <jeonjerry0709@gmail.com>
2024-09-09 16:22:38 +0900

    casting up,down casting down casting with dynamic_cast

--- Poke.cpp
+++ Poke.cpp
@@ -1,89 +1,89 @@
-#include <iostream>
-using namespace std;
-
-class Flyable {//¸ðµç ÇÔ¼ö°¡ virtual
-public:
-	virtual void fly() = 0;//pure virtual func
-	Flyable() {};
-	virtual ~Flyable() {}
-};
-
-class Pokemon {
-public:
-	Flyable* flyalbe;
-
-	Pokemon() {}
-
-	Pokemon(Flyable* flyable) {
-		this->flyalbe = flyable;
-	}
-	virtual ~Pokemon() {
-		cout << "Pokemon died\n";
-		delete flyalbe;
-		flyalbe = nullptr;
-
-	}
-	virtual void attack() {
-		cout << "attack!\n";
-	}
-	void performFly() {
-		flyalbe->fly();
-	}
-};
-class Pika :public Pokemon {
-public:
-	Pika() {};
-	Pika(Flyable* flyable) {
-		this->flyalbe = flyable;
-	}
-	~Pika() {
-		cout << "pikachu died\n";
-		delete this->flyalbe;
-		this->flyalbe = nullptr;
-	}
-	void attack() {
-		cout << "E-attack!\n";
-	}
-};
-class Wings :public Flyable{
-public:
-	void fly() {
-		cout << "ÆÄ´ßÆÄ´ß\n";
-	}
-	Wings() {
-		cout << "Wings created\n";
-	}
-	~Wings() {
-		cout << "Wings broken\n";
-	}
-};
-class Jetpack :public Flyable {
-public:
-	void fly() {
-		cout << "½¹\n";
-	}
-	Jetpack() {
-		cout<< "Jetpack created\n";
-	}
-	~Jetpack() {
-		cout << "Jetpack broken\n";
-	}
-};
-int main() {
-	Wings* wings = new Wings();
-	Pika* pika = new Pika(wings);
-	pika->attack();
-	pika->performFly();
-	delete pika;
-	pika = nullptr;
-
-	Pokemon* pika2 = new Pika(new Jetpack);
-	pika2->attack();
-	pika2->performFly();
-	delete pika2;
-	pika2 = nullptr;
-
-
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//class Flyable {//¸ðµç ÇÔ¼ö°¡ virtual
+//public:
+//	virtual void fly() = 0;//pure virtual func
+//	Flyable() {};
+//	virtual ~Flyable() {}
+//};
+//
+//class Pokemon {
+//public:
+//	Flyable* flyalbe;
+//
+//	Pokemon() {}
+//
+//	Pokemon(Flyable* flyable) {
+//		this->flyalbe = flyable;
+//	}
+//	virtual ~Pokemon() {
+//		cout << "Pokemon died\n";
+//		delete flyalbe;
+//		flyalbe = nullptr;
+//
+//	}
+//	virtual void attack() {
+//		cout << "attack!\n";
+//	}
+//	void performFly() {
+//		flyalbe->fly();
+//	}
+//};
+//class Pika :public Pokemon {
+//public:
+//	Pika() {};
+//	Pika(Flyable* flyable) {
+//		this->flyalbe = flyable;
+//	}
+//	~Pika() {
+//		cout << "pikachu died\n";
+//		delete this->flyalbe;
+//		this->flyalbe = nullptr;
+//	}
+//	void attack() {
+//		cout << "E-attack!\n";
+//	}
+//};
+//class Wings :public Flyable{
+//public:
+//	void fly() {
+//		cout << "ÆÄ´ßÆÄ´ß\n";
+//	}
+//	Wings() {
+//		cout << "Wings created\n";
+//	}
+//	~Wings() {
+//		cout << "Wings broken\n";
+//	}
+//};
+//class Jetpack :public Flyable {
+//public:
+//	void fly() {
+//		cout << "½¹\n";
+//	}
+//	Jetpack() {
+//		cout<< "Jetpack created\n";
+//	}
+//	~Jetpack() {
+//		cout << "Jetpack broken\n";
+//	}
+//};
+//int main() {
+//	Wings* wings = new Wings();
+//	Pika* pika = new Pika(wings);
+//	pika->attack();
+//	pika->performFly();
+//	delete pika;
+//	pika = nullptr;
+//
+//	Pokemon* pika2 = new Pika(new Jetpack);
+//	pika2->attack();
+//	pika2->performFly();
+//	delete pika2;
+//	pika2 = nullptr;
+//
+//
+//
+//	return 0;
+//}
---
+++ test_code.cpp
@@ -0,0 +1,45 @@
+/**************************************************************
+                    Dynamic cast
+ **************************************************************/
+#include <iostream>
+#include <string>
+#include <typeinfo> 
+using namespace std;
+
+class Animal {//Abstract class
+public:
+	virtual void run() = 0;
+};
+class Horse :public Animal{
+public:
+	void run() {
+		cout <<this<< "run with 4 legs Verrrrrrrry Fast\n";
+	}
+};
+class Racoon : public Animal {
+public:
+	void run() {
+		cout << "run slowly but Racoon is cute very cute\n";
+	}
+};
+
+int main()
+{
+	Animal* horse = new Horse();//UpCast
+	horse->run();
+
+	Animal* horsee = new Horse();
+	Horse* h = (Horse*)horsee;//Old style//DownCast
+	h->run();
+
+	Horse* h2 = dynamic_cast<Horse*>(horsee);//Modern style//DownCast
+	h2->run();
+
+	//Racoon* racoon = (Racoon*)horse;
+	/*Racoon* racoon = dynamic_cast<Racoon*>(horse);//type이 일치하지 않으면 null 대입*/
+	
+	
+	
+
+	return 0;
+}
