78e7fadcbedfcbc6b067182c19145e6e701a039a
jeon sungil <jeonjerry0709@gmail.com>
2024-09-12 16:05:20 +0900

    operator overloading v3 upgrade function with reference variable and const keyword

--- test_code.cpp
+++ test_code.cpp
@@ -19,8 +19,8 @@ public:
         cout << real_num << " + " << imaginary_num << "i\n";
     }
 };
-//General function
-Complex operator+(Complex left,Complex right) {
+//General function upgrade version
+Complex operator+(const Complex& left,const Complex& right) {//Using reference variable to save memory //add const(read only) to protect the original class
     int real = left.real_num + right.real_num;
     int imaginary = left.imaginary_num + right.imaginary_num;
 
@@ -32,8 +32,10 @@ int main() {
     Complex complex2(-3, 8);
     cout << complex.imaginary_num << endl;
 
+    //Complex complex3 = complex.operator+(complex2);//operating like this code in member function
+    //Complex complex3 = operator+(complex,complex2) //operating like this code in general function
     Complex complex3 = complex + complex2;
-    //Complex complex3 = complex.operator+(complex2);//operating like this code
+    
 
     complex3.printing();
 
