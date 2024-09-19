286fc0a220253719ae9eb9daf34148c4654f3620
jeon sungil <jeonjerry0709@gmail.com>
2024-09-19 15:38:54 +0900

    refator: use getter in operator+

--- test_code.cpp
+++ test_code.cpp
@@ -17,32 +17,42 @@ public:
         this->imaginary_num = imaginary_num;
     }
 
-    int real_num_setter(int real_num) { this->real_num = real_num; }
-    int real_num_getter() {return real_num;}
-    int imaginary_num_setter(int imaginary_num) { this->imaginary_num = imaginary_num; }
-    int imaginary_num_getter() { return imaginary_num; }
+    void real_num_setter(int real_num);
+    int real_num_getter() const;
+    void imaginary_num_setter(int imaginary_num);
+    int imaginary_num_getter() const;
 
-    //prefix operator
-    Complex operator++();
-    //postfix operator
-    Complex operator++(int);
+    Complex operator++();//prefix operator
+    Complex operator++(int);//postfix operator
 };
-//prefix operator
-Complex Complex::operator++() {
+void Complex::real_num_setter(int real_num){ this->real_num = real_num; }
+int Complex::real_num_getter() const { return real_num; }
+void Complex::imaginary_num_setter(int imaginary_num) { this->imaginary_num = imaginary_num; }
+int Complex::imaginary_num_getter() const { return imaginary_num; }
+
+
+
+Complex Complex::operator++() {//prefix operator
     return Complex(++this->real_num, this->imaginary_num);
 
     //this->real_num += 1;
     //return Complex(++this->real_num, this->imaginary_num);
 }
-//postfix operator
-Complex Complex::operator++(int) {
+
+Complex Complex::operator++(int) {//postfix operator
     Complex backup(this->real_num, this->imaginary_num);
     this->real_num++;
     return backup;
 }
 
-//Nonmember operator overloading
-ostream& operator<< (ostream& left, Complex& right) {
+Complex operator+(Complex& left,Complex& right) {
+    int real = left.real_num_getter() + right.real_num_getter();
+    int imaginary = left.imaginary_num_getter() + right.imaginary_num_getter();
+
+    return Complex(real, imaginary);
+}
+
+ostream& operator<< (ostream& left, Complex& right) {//Nonmember operator overloading
     string operator_buff = " +";
     if (right.imaginary_num_getter() < 0) {
         operator_buff = ' ';
@@ -58,5 +68,7 @@ int main() {
     cout << complex_2;
     Complex complex_3(5,-99);
     cout << complex_3;
+    Complex complex_4 = complex_1 + complex_3;
+    cout << complex_4;
     return 0;
 }
