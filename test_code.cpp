438456d0d23a9f6ab867fdb4bb40280fb7c37402
jeon sungil <jeonjerry0709@gmail.com>
2024-09-26 16:23:28 +0900

    add minus

--- test_code.cpp
+++ test_code.cpp
@@ -11,6 +11,7 @@ class Complex {//복소수
 private:
     int real_num;
     int imaginary_num;
+    string operator_buff = "+";
 
 public:
     Complex():real_num(0), imaginary_num(0){}
@@ -45,26 +46,27 @@ public:
 
     //relationship between ostream and Complex
     friend ostream& operator<< (ostream& out, Complex& right) {
-        string operator_buff = " +";
-        if (right.imaginary_num_getter() < 0) {
-            operator_buff = ' ';
-        }
 
-        out << right.real_num_getter() << operator_buff << right.imaginary_num_getter() << "i\n";
+        out << right.real_num_getter() << right.operator_buff << right.imaginary_num_getter() << "i\n";
         return out;
     }
     friend istream& operator>> (istream& in,Complex& complex) {//custom input
         string input;
         cout << "실수, 허수 입력\nex)10+9i\n";
         in >> input;
-        size_t plusIndex = input.rfind('+');
+        size_t operatorIndex = input.rfind('+');
         size_t imaginaryIndex = input.rfind('i');
+        if (operatorIndex == string::npos) {
+            operatorIndex = input.rfind('-');
+            complex.operator_buff = "-";
+        }
 
-        if (plusIndex != string::npos 
+
+        if (operatorIndex != string::npos 
             && imaginaryIndex != string::npos 
-            && imaginaryIndex > plusIndex) {
-            complex.real_num = stoi(input.substr(0, plusIndex));
-            complex.imaginary_num = stoi(input.substr(plusIndex + 1, imaginaryIndex));
+            && imaginaryIndex > operatorIndex) {
+            complex.real_num = stoi(input.substr(0, operatorIndex));
+            complex.imaginary_num = stoi(input.substr(operatorIndex + 1, imaginaryIndex-operatorIndex-1));
         }
         else {
             cout << "다시 입력해주세요";
