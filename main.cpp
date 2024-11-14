c86b2bccc689d03f5b098e68d01e19d9f0b3fa00
jeon sungil <jeonjerry0709@gmail.com>
2024-11-14 15:01:29 +0900

    pointer version templete

--- main.cpp
+++ main.cpp
@@ -1,15 +1,47 @@
-#include "DynamicArray.h"
+/**************************************************************
+ * 두 값을 스왑하는 템플릿 함수를 사용하는 프로그램           *
+ **************************************************************/
 #include <iostream>
 using namespace std;
 
-int main() {
-	try {
-		int* ptr = new int[5000000000];
-		delete ptr;
-		ptr = nullptr;
-		cout << "complete";
-	}
-	catch (const bad_alloc& e) {//catch allocation error
-		cout << "memory alloc error";
-	}
+// 템플릿 함수 정의
+//template <typename T>
+//void exchange(T& first, T& second)
+//{
+//	T temp = first;
+//	first = second;
+//	second = temp;
+//}
+
+// 템플릿 함수 정의
+template <typename t>
+void exchange(t* first, t* second)
+{
+	t temp = *first;
+	*first = *second;
+	*second = temp;
+}
+
+int main()
+{
+	// int 자료형 스왑
+	int integer1 = 5;
+	int integer2 = 70;
+	exchange(&integer1,& integer2);
+	cout << "swap(5, 70) = ";
+	cout << integer1 << "  " << integer2 << endl;
+	// double 자료형 스왑
+	double double1 = 101.5;
+	double double2 = 402.7;
+	exchange(&double1, &double2);
+	cout << "swap(101.5, 402.7) = ";
+	cout << double1 << "  " << double2 << endl;
+
+	// float 자료형 스왑
+	float float1 = 181.5f;
+	float float2 = 201.7f;
+	exchange(&float1, &float2);
+	cout << "swap(101.5, 402.7) = ";
+	cout << float1 << "  " << float2 << endl;
+	return 0;
 }
