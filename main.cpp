c77d82202350c8b2d6279afb85dd9f9dcd579ece
jeon sungil <jeonjerry0709@gmail.com>
2024-11-21 16:11:38 +0900

    file stream writing

--- main.cpp
+++ main.cpp
@@ -1,42 +1,28 @@
-#include "stack.cpp"
-#include "pokemon.h"
-#include <stdexcept>
-
-typedef Stack<Pokemon> PokemonStack;//별명 //Aliases
+#include <iostream>
+#include <fstream>
+#include <cassert>
+using namespace std;
 
 int main()
 {
-	try {
-		//Stack<Pokemon> poke_Stack(6);
-		PokemonStack poke_Stack(6);
+//    ofstream outStrm;
+//    // open file
+//    outStrm.open("protein.txt");
 
-		poke_Stack.push(Pokemon("파이리"));
-		poke_Stack.push(Pokemon("피카츄"));
-		poke_Stack.push(Pokemon("잠만보"));
-		poke_Stack.push(Pokemon("리자몽"));
-		poke_Stack.push(Pokemon("가이오가"));
-		poke_Stack.push(Pokemon("기라티나"));
-		//poke_Stack.push(Pokemon("단굴"));
+    ofstream outStrm("protein.txt");
 
-		for (int i = 0; i < 7; i++) {
-			cout << "가랏!" << poke_Stack.pop().get_poke() << "!!" << endl;
-		}
-	}
-	catch (const overflow_error e) {//save memory
-		cout << e.what() << endl;
-	}
-	catch (const out_of_range e) {
-		cout << e.what() << endl;
-	}
-	
-
-
-	//Stack<string> stack(20);
-	//stack.push("감자");
-	//stack.push("고구마");
-	//stack.push("옥수수");
-	//stack.push("알밤");
-	//cout << stack.pop() << endl;
-	//cout << stack.pop();
-	return 0;
+    if (!outStrm.is_open())
+    {
+        cout << "protein을 열 수 없습니다.";
+        assert(false);
+    }
+    // write to file
+    for (int i = 1; i <= 10; i++)
+    {
+        outStrm << i * 3 << "  ";
+    }
+    // 파일 닫기 
+    outStrm.close();
+    // after return ofstream 객체가 소멸
+    return 0;
 }
